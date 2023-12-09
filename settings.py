74a796f7d8f0e3c8f1e321858b0bf3d096774aa0
The-Akhilesh-Thapliyal <akhileshthapliyal20@gmail.com>
2023-12-09 18:32:03 +0530

    Tkinter-based MapApp with OpenStreetMap and Google Maps API integration.

---
+++ settings.py
@@ -0,0 +1,23 @@
+# Define map URLs
+MAIN_URL = "https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
+TERRAIN_URL = "https://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}&s=Ga"
+PAINT_URL = "http://c.tile.stamen.com/watercolor/{z}/{x}/{y}.png"
+
+# Define color-related constants
+COLOR_RANGE = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+ENTRY_BG = '#FFF'
+SIDE_PANEL_BG = '#EEE'
+HISTORY_HOVER_COLOR = '#AAA'
+BUTTON_COLOR = '#dbdbdb'
+BUTTON_HOVER_COLOR = '#C9C9C9'
+
+# Define text-related constants
+TEXT_FONT = 'Helvetica'
+TEXT_SIZE = 14
+TEXT_COLOR = '#000'
+CLOSE_ITEM_COLOR = '#555'
+
+# Define image paths
+map_image_path = 'images/map.png'
+paint_image_path = 'images/paint.png'
+terrain_image_path = 'images/terrain.png'
